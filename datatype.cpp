9bb6a95b8a0e59a22637f5dfa439d5f4bbf396a3
Nilufa Yesmin Ritu <126195882+programmer-nilufa@users.noreply.github.com>
2025-10-22 09:44:05 +0600

    datatype

---
+++ datatype.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float a = 34.12f;
+    int b = a;
+    cout << b << endl;
+
+    cout << sizeof a << endl; // lenth of variables
+
+    string name = "Nilufa";
+    cout << "My name is " << name << endl;
+
+    char grade = 'D';
+    cout << grade << endl;
+
+    bool is_like_me = true;
+    cout << is_like_me << endl;
+    return 0;
+}
