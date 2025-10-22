d299f6aeeaad88dc77e1abf44b7576c648a4bae2
Nilufa Yesmin Ritu <126195882+programmer-nilufa@users.noreply.github.com>
2025-10-22 09:44:27 +0600

    logical_op

---
+++ logical_op.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout << "Enter the frist number:";
+    cin >> a;
+    cout << "Enter the second number:";
+    cin >> b;
+
+    cout << "Sum of two number is:" << (a+b) <<endl;
+    cout << "Subtract of two number is:" <<(a-b) <<endl;
+    cout << "Product of two number is:" << (a*b) <<endl;
+    cout << "Divission of two number is:" <<(a/b) <<endl;
+    cout << "Modulus of two number is:" <<(a%b) <<endl;
+
+    return 0;
+} 
