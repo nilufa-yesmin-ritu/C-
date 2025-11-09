9d9e94c8f86a9c54a63fc1cab9dcb4693ca94208
Nilufa Yesmin Ritu <126195882+nilufa-yesmin-ritu@users.noreply.github.com>
2025-11-09 10:46:32 +0600

    array_index

---
+++ arr_index.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main (){
+    cout << "Enter the array size";
+    int size;
+    cin >> size;
+
+    cout << "Enter the array element";
+    int arr[size];
+    for(int i=0; i<size; i++){
+        cin >> arr[i];
+    }
+
+    for(int i=0; i<size; i++){
+        
+    }
+    cout << endl;
+    return 0;
+}
