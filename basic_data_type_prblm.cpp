c5ce937ec2eccf17918ad2a981a55e28b7078aca
Nilufa Yesmin Ritu <126195882+nilufa-yesmin-ritu@users.noreply.github.com>
2026-01-03 22:36:35 +0600

    basic_data_type_prblm

---
+++ basic_data_type_prblm.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include<iomanip>
+using namespace std;
+int main(){
+int i; long l; char ch; float f; double d;
+    cin >> i >> l >> ch >> f >> d;
+
+    cout << i << endl << l << endl << ch << endl << fixed << setprecision(3) << f << endl <<  fixed << setprecision(9) << d  << endl;   
+    return 0;
+}
