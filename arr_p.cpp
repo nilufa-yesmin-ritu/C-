04ad3e05e407100e3599e7f0a898ccb451a81efc
Nilufa Yesmin Ritu <126195882+nilufa-yesmin-ritu@users.noreply.github.com>
2026-01-26 09:21:23 +0600

    arr_p

--- arr_p.cpp
+++ arr_p.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
 using namespace std;
 int main (){
-    cout << "Enter the lenth of array: ";
-    int len;
-    cin >> len;
+    // cout << "Enter the lenth of array: ";
+    // int len;
+    // cin >> len;
 
-    cout << "Enter the array element: ";
-    int arr[len];
-    for(int i=0; i<len; i++){
-        cin >> arr[i];
-    }
+    // cout << "Enter the array element: ";
+    // int arr[len];
+    // for(int i=0; i<len; i++){
+    //     cin >> arr[i];
+    // }
 
-    for(int i=0; i<len; i++){
-        cout << arr[i] << " ";
+    // for(int i=0; i<len; i++){
+    //     cout << arr[i] << " ";
+    // }
+    // cout << endl;
+
+    int arr[5] = {2,6,8,9,1};
+    
+    for(int i=0; i<5; i++){
+        cout << arr[i] <<' ';
     }
-    cout << endl;
 
     return 0;
 }
